cac9a981ae5f00ea391798125d67f0a0b360f9eb
Alexandra Kowalewska <kowalewska.alexandra@gmail.com>
2017-12-04 19:42:50 +0100

    calculator - changes

--- js/calculator.js
+++ js/calculator.js
@@ -1,4 +1,3 @@
-
 function countProfit(payment, numberOfYears, capitalizationPeriod, interestRate, tax) {
     
     /*if (tax) {
@@ -9,9 +8,11 @@ function countProfit(payment, numberOfYears, capitalizationPeriod, interestRate,
     
     var taxedInterestRate = (tax)?(interestRate*0.81):interestRate;
     
-    var capitalTot = payment * Math.pow(1 + (taxedInterestRate/capitalizationPeriod),numberOfYears*capitalizationPeriod);
+    var total = payment * Math.pow(1 + (taxedInterestRate/capitalizationPeriod),numberOfYears * capitalizationPeriod);
     
-    return capitalTot.toFixed(2);
+    return total.toFixed(2);
+    
+    
 }
 
 
@@ -19,13 +20,13 @@ function calculate() {
     
     /* Pobieranie zmiennych z formularza i parsowanie do floata */
     var payment = parseFloat(document.getElementById('payment').value);
-    var numberOfYears = parseFloat(document.getElementById('number-of-years'). value);
+    var numberOfYears = parseFloat(document.getElementById('number-years'). value);
     var capitalizationPeriod = parseFloat(document.getElementById('capitalization-period').value);
     var interestRate = parseFloat(document.getElementById('interest-rate').value/100);
     var tax = document.getElementById('tax').checked;
     
     /* Przypisywanie wyniku obliczania podatku do zmiennej wynik */
-    var result = countProfit(payment, numberOfYears, interestRate, tax, capitalizationPeriod);
+    var result = countProfit(payment, numberOfYears, capitalizationPeriod, interestRate, tax);
     
     document.getElementById('result').innerHTML = 'You will get ' + result + 'pln';
 }
